6ef32da85b7de14845ba6f72e00ad24d1ab4e882
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-08-18 09:33:49 +0100

    nvim: add compiler.nvim

--- dotfiles/nvim/lua/core/lazy.lua
+++ dotfiles/nvim/lua/core/lazy.lua
@@ -80,6 +80,27 @@ local plugins = {
     }
   },
 
+  -- Compiler nvim ()
+  {
+    "Raiden1411/compiler.nvim",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+    opts = {},
+  },
+  {
+    "stevearc/overseer.nvim",
+    commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1
+      },
+    },
+  },
+
   -- LuaSnip (https://github.com/L3MON4D3/LuaSnip)
   {
     "L3MON4D3/LuaSnip",
